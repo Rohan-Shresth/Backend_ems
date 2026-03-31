0268916deb3fba937293d969167427913b04301d
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-03-31 13:01:21 +0545

    Registration page

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Register from "./pages/Register";
+import "./index.css";
+
+function App() {
+  return (
+    <div className=" bg-[#F8FAFC] min-h-screen text-black overflow-hidden">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
